f8f8b80e6867a1103866b16b61ce440578212efb
Juan Pablo Diaz <juan.diaz93@hotmail.com>
2025-07-15 15:12:15 -0400

    fix: Update

--- server.ts
+++ server.ts
@@ -1,18 +1,18 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
+import bodyParser from "body-parser";
 
-import setupWebSocketServer from './server/websocket-server';
-import ollamaChatRequest from './server/ollama-route';
-import setupMCPServer, { sseGet, messagesPost } from './server/mcpServer';
-import { port } from './config';
+import setupWebSocketServer from "./server/websocket-server";
+import ollamaChatRequest from "./server/ollama-route";
+import setupMCPServer from "./server/mcpServer";
+import { port } from "./config";
 
 const app = express();
 const server = app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`✅ Server is running on http://localhost:${port}`);
 });
-const jsonParser = bodyParser.json()
+const jsonParser = bodyParser.json();
 
-app.use(express.static('webapp'));
+app.use(express.static("webapp"));
 const webSocketServer = setupWebSocketServer(server);
 setupMCPServer(webSocketServer, app);
-app.post('/api/request', jsonParser, ollamaChatRequest);
+app.post("/api/request", jsonParser, ollamaChatRequest);
